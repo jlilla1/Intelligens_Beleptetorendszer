bbe05f6103d856f4763e22c4b7e95b929b6c93f6
jlilla1 <45033736+jlilla1@users.noreply.github.com>
2022-11-15 22:26:04 +0100

    Face Reognition
    Some changes have been made on the original code.
    The original face recognition program can be found here: https://github.com/joeVenner/FaceRecognition-GUI-APP

---
+++ FaceRec/CreateDatasetPy.py
@@ -0,0 +1,6 @@
+from create_dataset import start_capture
+import sys
+
+
+name = sys.argv[1]
+start_capture(name)
---
+++ FaceRec/TrainDatasetPy.py
@@ -0,0 +1,6 @@
+from create_classifier import train_classifer
+import sys
+
+
+name = sys.argv[1]
+train_classifer(name)
